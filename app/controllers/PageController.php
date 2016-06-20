f982bc59351f58d44bc9f4ff09d808e94e536fe5
Ben Abbott <jaminscript@gmail.com>
2016-06-21 11:16:45 +1200

    Show products on stream page

--- app/controllers/PageController.php
+++ app/controllers/PageController.php
@@ -5,6 +5,14 @@ abstract class PageController {
 	protected $title;
 	protected $metaDesc;
 	protected $dbc;
+	protected $plates;
+
+	public function __construct() {
+
+		// Instantiate (create instance of) Plates library
+		$this->plates = new League\Plates\Engine('app/templates');
+
+	}
 
 	// Force children classes to have the buildHTML function
 	abstract public function buildHTML();
--- app/controllers/StreamController.php
+++ app/controllers/StreamController.php
@@ -7,6 +7,10 @@ class StreamController extends PageController {
 
 	// Constructor
 	public function __construct($dbc) {
+
+		// Run the parent constructor
+		parent::__construct();
+
 		$this->dbc = $dbc;
 
 		// If you are not logged in
@@ -19,6 +23,38 @@ class StreamController extends PageController {
 	// Methods (functions)
 	public function buildHTML() {
 
+		// Get latest posts (pins)
+		$allData = $this->getLatestPosts();
+
+		$data = [];
+
+		$data['allPosts'] = $allData;
+
+		echo $this->plates->render('stream', $data);
+
+	}
+
+	private function getLatestPosts() {
+
+		// Prepare some SQL
+		$sql = "SELECT *
+				FROM posts";
+
+		// Run the SQL and capture the result
+		$result = $this->dbc->query($sql);
+
+		// Extract the results as an array
+		$allData = $result->fetch_all(MYSQLI_ASSOC);
+
+		// Return the results to the code that called this function
+		return $allData;
+
 	}
 
 }
+
+
+
+
+
+
